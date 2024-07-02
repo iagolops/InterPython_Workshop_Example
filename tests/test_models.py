70c09623b0b2aaa1d3a21962ceca734b7616c68c
iago <iagolops2012@gmail.com>
2024-07-02 11:29:00 -0300

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,26 +11,47 @@ def test_max_mag_strings():
     with pytest.raises(TypeError):
         error_expected = max_mag('string', test_input_colname)
 
-def test_max_mag_integers():
-    # Test that max_mag function works for integers
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        7),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+    ])
+
+def test_max_mag(test_df, test_colname, expected):
+    """Test max function works for array of zeroes and positive integers."""
     from lcanalyzer.models import max_mag
+    assert max_mag(test_df, test_colname) == expected
 
-    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
-                                       [7, 8, 9], 
-                                       [3, 4, 1]], columns=list("abc"))
-    test_input_colname = "a"
-    test_output = 7
 
-    assert max_mag(test_input_df, test_input_colname) == test_output
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        pytest.approx(3.66,0.1)),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 1, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        1/3),
+    ])
 
-def test_max_mag_zeros():
-    # Test that max_mag function works for zeros
-    from lcanalyzer.models import max_mag
+def test_mean_mag(test_df, test_colname, expected):
+    from lcanalyzer.models import mean_mag
+    assert mean_mag(test_df, test_colname) == expected
 
-    test_input_df = pd.DataFrame(data=[[0, 0, 0], 
-                                       [0, 0, 0], 
-                                       [0, 0, 0]], columns=list("abc"))
-    test_input_colname = "b"
-    test_output = 0
-
-    assert max_mag(test_input_df, test_input_colname) == test_output
