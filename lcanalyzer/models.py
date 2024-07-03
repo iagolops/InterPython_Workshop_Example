8ceb28a46a3d8f33021ab16611bb23b37abe1316
iago <iagolops2012@gmail.com>
2024-07-03 12:25:18 -0300

    Correct requirements

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -11,18 +11,19 @@ import pandas as pd
 import numpy as np
 from astropy.timeseries import LombScargle
 
+
 def load_dataset(filename):
     """Load a table from CSV file.
-    
+
     :param filename: The name of the .csv file to load
     :returns: pd.DataFrame with the data from the file.
     """
     return pd.read_csv(filename)
 
 
-def mean_mag(data,mag_col):
+def mean_mag(data, mag_col):
     """Calculate the mean magnitude of a lightcurve periods
-    
+
     :param data: The data frame
     :param mag_col: a string with the name of the column for calculating the mean value
     :returns: A float with the mean value of the column.
@@ -30,9 +31,9 @@ def mean_mag(data,mag_col):
     return data[mag_col].mean()
 
 
-def max_mag(data,mag_col):
+def max_mag(data, mag_col):
     """Calculate the max magnitude of a lightcurve periods
-    
+
     :param data: pd.DataFrame with the magnitudes
     :param mag_col: a string with the name of the column for calculating the max value
     :returns: A float with the max value of the column.
@@ -40,11 +41,32 @@ def max_mag(data,mag_col):
     return data[mag_col].max()
 
 
-def min_mag(data,mag_col):
+def min_mag(data, mag_col):
     """Calculate the min magnitude of a lightcurve periods
-    
+
     :param data: pd.DataFrame with observed magnitudes for a single source
     :param mag_col: a string with the name of the column for calculating the min value
     :returns: A float with the min value of the column.
     """
     return data[mag_col].min()
+
+
+def calc_stats(lc, bands, mag_col):
+    # Calculate max, mean and min values for all bands of a light curve
+    stats = {}
+    for b in bands:
+        stat = {}
+        stat["max"] = max_mag(lc[b], mag_col)
+        stat["mean"] = mean_mag(lc[b], mag_col)
+        stat["min"] = min_mag(lc[b], mag_col)
+        stats[b] = stat
+    return pd.DataFrame.from_records(stats)
+
+
+def normalize_lc(df, mag_col):
+    # Normalize a single light curve
+    min = min_mag(df, mag_col)
+    max = max_mag((df - min), mag_col)
+    lc = (df[mag_col] - min) / max
+    lc = lc.fillna(0)
+    return lc
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import pytest
+import pandas.testing as pdt
 
 def test_max_mag_strings():
     # Test for TypeError when passing a string
@@ -44,14 +45,93 @@ def test_max_mag(test_df, test_colname, expected):
         "a",
         pytest.approx(3.66,0.1)),
         (pd.DataFrame(data=[[0, 0, 0], 
-                            [0, 1, 0], 
+                            [0, 0, 0], 
                             [0, 0, 0]], 
                       columns=list("abc")),
         "b",
-        1/3),
+        0),
     ])
 
 def test_mean_mag(test_df, test_colname, expected):
     from lcanalyzer.models import mean_mag
     assert mean_mag(test_df, test_colname) == expected
+
+
+
+test_cols = list('abc')
+test_dict = {}
+test_dict["df0"] = pd.DataFrame(
+    data=[[8, 8, 0], 
+          [0, 1, 1], 
+          [2, 3, 1], 
+          [7, 9, 7]], columns=test_cols
+)
+test_dict["df1"] = pd.DataFrame(
+    data=[[3, 8, 2], 
+          [3, 8, 0], 
+          [3, 9, 8], 
+          [8, 2, 5]], columns=test_cols
+)
+test_dict["df2"] = pd.DataFrame(
+    data=[[8, 4, 3], 
+          [7, 6, 3], 
+          [4, 2, 9], 
+          [6, 4, 0]], columns=test_cols
+)
+
+@pytest.mark.parametrize(
+    "test_lc, test_bands, test_mag_col, expected",
+    [
+        (
+            test_dict,
+            ['df0', 'df1', 'df2'],
+            'b',
+            pd.DataFrame(data=[[9, 9, 6], 
+                               [5.25, 6.75, 4.0], 
+                               [1, 2, 2]],
+                         columns=['df0', 'df1', 'df2'],
+                         index=['max', 'mean', 'min']).astype('float64')
+        )
+    ]
+)
+
+
+def test_calc_stats(test_lc, test_bands, test_mag_col, expected):
+    from lcanalyzer.models import calc_stats
+    pdt.assert_frame_equal(calc_stats(test_lc, test_bands, 'b'),
+                              expected,
+                             check_exact=False,
+                             atol=0.01)
+
+# Parametrization for normalize_lc function testing
+@pytest.mark.parametrize(
+    "test_input_df, test_input_colname, expected",
+    [
+        (pd.DataFrame(data=[[8, 9, 1], 
+                            [1, 4, 1], 
+                            [1, 2, 4], 
+                            [1, 4, 1]], 
+                      columns=list("abc")),
+        "b",
+        pd.Series(data=[1,0.285,0,0.285])),
+        (pd.DataFrame(data=[[1, 1, 1], 
+                            [1, 1, 1], 
+                            [1, 1, 1], 
+                            [1, 1, 1]], 
+                      columns=list("abc")),
+        "b",
+        pd.Series(data=[0.,0.,0.,0.])),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+         pd.Series(data=[0.,0.,0.,0.])),
+    ])
+def test_normalize_lc(test_input_df, test_input_colname, expected):
+    """Test how normalize_lc function works for arrays of positive integers."""
+    from lcanalyzer.models import normalize_lc
+    import pandas.testing as pdt
+    pdt.assert_series_equal(normalize_lc(test_input_df,test_input_colname),expected,check_exact=False,atol=0.01,check_names=False)
 
